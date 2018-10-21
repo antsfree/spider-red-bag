9b384c611ccedaae6d2fddd765621a9348027e15
antsfree <antsfreedom@gmail.com>
2018-10-21 22:12:00 +0800

    shell 更新；

--- init.sh
+++ init.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
+# 先清除进程
+pkill -f receive_red_bag.py
+pkill -f click_city_box.py
+pkill -f click_red_bag.py
+echo '初始化完成...'
+
 # 启动红包获取程序
 ./receive_red_bag.py &
+echo '红包进程开启中...'
 
+# 预留时间初始化表
 sleep 30s
-echo '初始化完成...'
 
 # 启动格子广告红包程序
 ./click_city_box.py &
@@ -14,4 +21,4 @@ echo '格子广告启动成功'
 ./click_red_bag.py &
 echo '红包消费启动成功'
 
-echo '完成'
+echo '完成!'
