4410064e3ec21ec122e96b8a7c06adf1d958b13a
antsfree <antsfreedom@gmail.com>
2018-11-01 15:22:26 +0800

    update

--- email_message.py
+++ email_message.py
@@ -5,32 +5,33 @@ import time
 import config
 from user_info import get_user_list
 
+
 def send_email():
     """
     发邮件
     :return:
     """
     try:
-        #qq邮箱smtp服务器
+        # qq邮箱smtp服务器
         host_server = config.MAIL_HOST
-        #sender_qq为发件人的qq号码
+        # sender_qq为发件人的qq号码
         sender_qq = config.MAIL_USER
-        #pwd为qq邮箱的授权码
+        # pwd为qq邮箱的授权码
         pwd = config.MAIL_PASS
-        #发件人的邮箱
+        # 发件人的邮箱
         sender_qq_mail = config.SENDER_EMAIL
-        #收件人邮箱
+        # 收件人邮箱
         receiver = config.RECEIVER_EMAIL
-        #邮件的正文内容
+        # 邮件的正文内容
         mail_content = str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())) + ' 脚本已经跑完, 进入下个周期。'
         user_list = str(get_user_list('user_name,user_money'))
         mail_content = mail_content + "\n" + user_list
-        #邮件标题
+        # 邮件标题
         mail_title = '脚本处理结果报告'
 
-        #ssl登录
+        # ssl登录
         smtp = SMTP_SSL(host_server)
-        #set_debuglevel()是用来调试的。参数值为1表示开启调试模式，参数值为0关闭调试模式
+        # set_debuglevel()是用来调试的。参数值为1表示开启调试模式，参数值为0关闭调试模式
         smtp.set_debuglevel(1)
         smtp.ehlo(host_server)
         smtp.login(sender_qq, pwd)
